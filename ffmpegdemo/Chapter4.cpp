7bd20984560d384936677e4168f8dcbbcf6c04d7
Stan Hu <3421902@qq.com>
2019-10-27 00:33:38 +0800

    can change new row

--- ffmpegdemo/Chapter4.cpp
+++ ffmpegdemo/Chapter4.cpp
@@ -6,16 +6,54 @@
 using namespace cv;
 using namespace std;
 
+//如果要做到可以删字符，不能用这种开发方式，要用保存字符串的方式
+Mat current;
 void practise1() {
-	namedWindow("no1", WINDOW_AUTOSIZE);
-	Mat m = Mat(500, 500, CV_8UC3, Scalar(0, 0, 0));
-	imshow("no1", m);
+	current = Mat::zeros(500, 500, CV_8UC3);
+	imshow("dstImage", current);
+	int left = 10;
+	int top = 20;
+	 while (true)
+	 {
+		 int s = waitKey(0);
+		 if (s == 13) {
+			 top += 25;
+			 left = 10;
+			 continue;
+		 }
+		 if (s == 8) {
+			 if (left <= 10 && top <= 20){
+				 continue;
+			 }
+			 else if (left <= 10 && top >= 20) {
+				 left = 475;
+				 top = top - 25;
+			 }
+			
+			 continue;
+		 }
+		 const char c = (char)s;
+		 cout << c << endl;
+		 putText(current, string(1,c), Point(left, top), FONT_HERSHEY_SIMPLEX, 1, Scalar::all(255));
+		 imshow("dstImage", current);
+		 left += 15;
+		 if (left >= 485) {
+			 top += 25;
+			 left = 10;
+		 }
+		 if (left >= 485 && top >= 490) {
+			 break;
+		 }
+	 }
 }
+
+
 
 
 int learnOpenCV4()
 {
 	practise1();
+	
 	waitKey(0);
 	return 0;
 }
