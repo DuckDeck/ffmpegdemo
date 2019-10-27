2146d54d700c89756fd9ffcfb87d159c426b61e9
Stan Hu <3421902@qq.com>
2019-10-27 14:25:35 +0800

    process code

--- ffmpegdemo/Chapter4.cpp
+++ ffmpegdemo/Chapter4.cpp
@@ -47,12 +47,56 @@ void practise1() {
 	 }
 }
 
+string content ;
+
+//这种方法对于要换行很不友好，还是要用string数组
+void practice1() {
+	
+	int numOfLines = 32;
+
+	int top = 20;
+	while (true)
+	{
+		current = Mat::zeros(500, 500, CV_8UC3);
+		int s = waitKey(0);
+		if (s == 13) { //用这种方便这下麻烦了,看来最好弄一个string数据
+			top += 25;
+			
+			continue;
+		}
+		
+		const char c = (char)s;
+		content += c;
+		//根据有多少内容画多少行
+		int length = content.length();
+		string tmp = content.substr(0, numOfLines);
+		int i = 1;
+		while (tmp.length() == numOfLines)
+		{
+			
+			putText(current, tmp, Point(5, top), FONT_HERSHEY_SIMPLEX, 1, Scalar::all(255));
+			imshow("dstImage", current);
+			
+			tmp = content.substr(i * numOfLines, numOfLines);
+			i += 1;
+			top = top +  25;
+		}
+		putText(current, tmp, Point(5, top), FONT_HERSHEY_SIMPLEX, 1, Scalar::all(255));
+		imshow("dstImage", current);
+		
+		if (top >= 490) {
+			break;
+		}
+		top = 20; //要复位一下
+	}
+	
+}
 
 
 
 int learnOpenCV4()
 {
-	practise1();
+	practice1();
 	
 	waitKey(0);
 	return 0;
